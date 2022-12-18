f902074b4fcd845fe4fe6ffa021f73c7fdc4971c
Misan Etchie <msnetchie11@gmail.com>
2022-12-18 09:57:49 +0100

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Misan Etchie on 18/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
